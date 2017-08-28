8084161912d10000b1862a1cdfb32a6e2baf7620
Matthew Wagerfield <matthew@wagerfield.com>
2017-08-28 21:10:17 +0100

    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@ const babel = require('gulp-babel')
 const gulp = require('gulp')
 const del = require('del')
 
+const srcGlob = 'src/**/*.js'
+const destDir = 'dist'
+
 gulp.task('clean', () => {
-  return del('dist')
+  return del(destDir)
 })
 
 gulp.task('build', [ 'clean' ], () => {
-  return gulp
-    .src('src/*.js')
+  return gulp.src(srcGlob)
     .pipe(babel())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest(destDir))
+})
+
+gulp.task('watch', [ 'build' ], () => {
+  return gulp.watch(srcGlob, [ 'build' ])
 })
